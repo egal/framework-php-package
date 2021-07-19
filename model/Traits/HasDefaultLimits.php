7661a0f188df5fa3980926e8a8962b5960043da8
rollsover <51847757+rollsover@users.noreply.github.com>
2021-07-19 17:46:37 +0700

    Feature: Limits for count simultaneous manipulations with entities in actions. (#53)
    * Feature: Limits for count simultaneous manipulations with entities in actions.
    
    * Update: Rename HasDefaultLimits::maxCountEntitiesCanToManipulateWithAction => HasDefaultLimits::maxCountEntitiesToProcess.
    
    * Update: Code Style Refactoring.
    
    * Fix: Model typos.

--- model/Traits/HasDefaultLimits.php
+++ model/Traits/HasDefaultLimits.php
@@ -1,7 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Egal\Model\Traits;
 
+use Egal\Model\Exceptions\ExceedingTheLimitCountEntitiesForManipulationException;
+
+/**
+ * Trait HasDefaultLimits.
+ *
+ * Содержит в себе ограничения при использовании actions у {@see \Egal\Model\Model}.
+ */
 trait HasDefaultLimits
 {
 
@@ -14,11 +23,51 @@ trait HasDefaultLimits
     protected int $maxDisplayedCount = 100;
 
     /**
-     * @return int
+     * The maximum count of entities that can be manipulated using actions
+     *
+     * Предназначено для защиты от высоких нагрузок на систему.
+     * При изменении данного параметра, вся ответственность по производительности лежи на разработчике.
+     */
+    protected int $maxCountEntitiesToProcess = 10;
+
+    /**
+     * Getter for {@see \Egal\Model\Traits\HasDefaultLimits::maxDisplayedCount}.
      */
     public function getMaxDisplayedCount(): int
     {
         return $this->maxDisplayedCount;
+    }
+
+    /**
+     * Getter for {@see \Egal\Model\Traits\HasDefaultLimits::maxCountEntitiesToProcess}.
+     */
+    public function getMaxCountEntitiesToProcess(): int
+    {
+        return $this->maxCountEntitiesToProcess;
+    }
+
+    /**
+     * Checks if {@param $count} is less than {@see \Egal\Model\Traits\HasDefaultLimits::maxCountEntitiesToProcess}.
+     */
+    public function isLessThanMaxCountEntitiesCanToManipulateWithAction(int $count): bool
+    {
+        return $count < $this->getMaxCountEntitiesToProcess();
+    }
+
+    /**
+     * Checks if {@param $count} is less than {@see \Egal\Model\Traits\HasDefaultLimits::maxCountEntitiesToProcess}.
+     *
+     * Otherwise, throws an exception.
+     *
+     * @throws \Egal\Model\Exceptions\ExceedingTheLimitCountEntitiesForManipulationException
+     */
+    public function isLessThanMaxCountEntitiesCanToManipulateWithActionOrFail(int $count): bool
+    {
+        if (!$this->isLessThanMaxCountEntitiesCanToManipulateWithAction($count)) {
+            throw new ExceedingTheLimitCountEntitiesForManipulationException();
+        }
+
+        return true;
     }
 
 }
