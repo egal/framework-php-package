34273c3d6a3ece066ff052da9aad36cd37150f30
rollsover <ro.rollsover@gmail.com>
2021-07-30 16:50:31 +0700

    Update: Code Style auto fix.

--- model/Traits/UsesValidator.php
+++ model/Traits/UsesValidator.php
@@ -1,28 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Egal\Model\Traits;
 
 use Egal\Model\Exceptions\ValidateException;
 use Egal\Model\Model;
 use Illuminate\Support\Facades\Validator;
-use ReflectionException;
 
-/**
- * Trait UsesValidator
- * @package Egal\Model
- */
 trait UsesValidator
 {
 
     /**
-     * @throws ValidateException
-     * @throws ReflectionException
-     * @noinspection PhpUnused
+     * @throws \Egal\Model\Exceptions\ValidateException
+     * @throws \ReflectionException
      */
-    protected static function bootUsesValidator()
+    protected static function bootUsesValidator(): void
     {
-        static::saving(function ($entity) {
-            /** @var Model $entity */
+        static::saving(static function (Model $entity): void {
             // Получаем все измененные атрибуты
             $attributes = $entity->getDirty();
 
@@ -38,15 +33,17 @@ trait UsesValidator
 
             // Применяем полученные validation rules на все атрибуты модели
             $validator = Validator::make($entity->getAttributes(), $validationRules);
+
             if ($validator->fails()) {
                 $exception = new ValidateException();
                 $exception->setMessageBag($validator->errors());
+
                 throw $exception;
             }
         });
     }
 
-    private function validateKey($keyValue)
+    private function validateKey($keyValue): void
     {
         $validator = Validator::make(
             [$this->getKeyName() => $keyValue],
@@ -56,6 +53,7 @@ trait UsesValidator
         if ($validator->fails()) {
             $exception = new ValidateException();
             $exception->setMessageBag($validator->errors());
+
             throw $exception;
         }
     }
