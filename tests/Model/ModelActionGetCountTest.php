315daf3d9c6ff937f4ba7e1610ad4762a6af7edc
glebstorozhenko <ro.rollsover@gmail.com>
2021-08-10 20:07:46 +0700

    Feature: Model::actionGetCount().

---
+++ tests/Model/ModelActionGetCountTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Egal\Tests\Model;
+
+use Egal\Model\Filter\FilterConditions\SimpleFilterConditionApplier;
+use Egal\Model\Metadata\ModelMetadata;
+use Egal\Model\Model;
+use Egal\Tests\DatabaseSchema;
+use Illuminate\Database\Schema\Blueprint;
+use PHPUnit\Framework\TestCase;
+
+class ModelActionGetCountTest extends TestCase
+{
+
+    use DatabaseSchema;
+
+    protected function createSchema(): void
+    {
+        $this->schema()->create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        $productsAttributes = [
+            ['id' => 1],
+            ['id' => 2],
+            ['id' => 3],
+            ['id' => 4],
+        ];
+
+        foreach ($productsAttributes as $attributes) {
+            ModelActionGetCountTestProductStub::create($attributes);
+        }
+    }
+
+    protected function dropSchema(): void
+    {
+        $this->schema()->drop('products');
+    }
+
+    public function productsFilterDataProvider()
+    {
+        return [
+            [
+                [],
+                null,
+                4,
+            ],
+            [
+                [
+                    ['id', 'eq', 5],
+                ],
+                null,
+                0,
+            ],
+            [
+                [
+                    ['id', 'eq', 1],
+                ],
+                null,
+                1,
+            ],
+            [
+                [
+                    ['id', 'eq', 1],
+                    'OR',
+                    ['id', 'eq', 2],
+                ],
+                null,
+                2,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider productsFilterDataProvider
+     */
+    public function testProductsFilter(?array $filter, ?string $expectException, int $equalsExpect)
+    {
+        if ($expectException) {
+            $this->expectException($expectException);
+        }
+
+        $actual = ModelActionGetCountTestProductStub::actionGetCount($filter)['count'];
+
+        if ($equalsExpect) {
+            $this->assertEquals($equalsExpect, $actual);
+        }
+    }
+
+}
+
+class ModelActionGetCountTestProductStub extends Model
+{
+
+    protected $table = 'products';
+    protected $guarded = [];
+    protected $fillable = [];
+
+    public function getModelMetadata(): ModelMetadata
+    {
+        return new ModelMetadata(static::class);
+    }
+
+}
