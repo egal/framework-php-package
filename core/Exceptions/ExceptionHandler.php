89433607da29ca339b25766018de72755b8a0ff8
glebstorozhenko <ro.rollsover@gmail.com>
2021-08-19 22:47:02 +0700

    Fix: Stopping listener while processing messages when throws some exception.

--- core/Exceptions/ExceptionHandler.php
+++ core/Exceptions/ExceptionHandler.php
@@ -1,15 +1,13 @@
-<?php /** @noinspection PhpMissingFieldTypeInspection */
+<?php
+
+declare(strict_types=1);
 
 namespace Egal\Core\Exceptions;
 
 use Egal\Core\Bus\Bus;
 use Egal\Core\Messages\ActionErrorMessage;
 use Egal\Core\Session\Session;
-use Exception;
 use Illuminate\Database\QueryException;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Laravel\Lumen\Exceptions\Handler as BaseExceptionHandler;
 use Throwable;
 
@@ -17,58 +15,30 @@ class ExceptionHandler extends BaseExceptionHandler
 {
 
     /**
-     * Список типов исключений, о которых не следует сообщать.
-     *
-     * @var array
+     * @var string[]
      */
     protected $dontReport = [];
 
-    /**
-     * Сообщить или записать исключение.
-     *
-     * Это отличное место для отправки исключений в Sentry, Bugsnag и т. Д.
-     *
-     * @param Throwable $e
-     * @return void
-     *
-     * @throws Exception
-     */
-    public function report(Throwable $e)
+    public function report(Throwable $exception)
     {
         if (Session::isActionMessageExists()) {
             $actionErrorMessage = new ActionErrorMessage();
+            $actionErrorMessage->setMessage($exception->getMessage());
 
-            switch (get_class($e)) {
+            switch (get_class($exception)) {
                 case QueryException::class:
-                    /** @var QueryException $e */
-                    $actionErrorMessage->setMessage($e->getMessage());
                     $actionErrorMessage->setCode(500);
                     break;
                 default:
-                    $actionErrorMessage->setMessage($e->getMessage());
-                    $actionErrorMessage->setCode($e->getCode());
+                    $actionErrorMessage->setCode($exception->getCode());
                     break;
             }
 
             $actionErrorMessage->setActionMessage(Session::getActionMessage());
             Bus::getInstance()->publishMessage($actionErrorMessage);
-            Bus::getInstance()->destructEnvironment();
         }
-        parent::report($e);
-    }
 
-    /**
-     * Вывести исключение в HTTP-ответ.
-     *
-     * @param Request $request
-     * @param Throwable $e
-     * @return Response|JsonResponse
-     *
-     * @throws Throwable
-     */
-    public function render($request, Throwable $e)
-    {
-        return parent::render($request, $e);
+        parent::report($exception);
     }
 
 }
