b0082dd03de0db9113249718d53a0e0c49527901
rollsover <51847757+rollsover@users.noreply.github.com>
2021-09-06 16:15:44 +0700

    Update: Added snake case validation of property variable name. (#123)

--- tests/Model/ModelMetadataTest.php
+++ tests/Model/ModelMetadataTest.php
@@ -2,37 +2,66 @@
 
 namespace Egal\Tests\Model;
 
+use Egal\Core\Application;
+use Egal\Model\Exceptions\IncorrectCaseOfPropertyVariableNameException;
 use Egal\Model\Metadata\ModelMetadata;
 use Egal\Model\Model;
+use Illuminate\Support\Facades\Config;
 use PHPUnit\Framework\TestCase;
 
 class ModelMetadataTest extends TestCase
 {
 
-    public function testGetValidationRules()
+    protected function setUp(): void
     {
-        $this->assertEquals(
-            [
-                'id' => ['integer'],
-                'foo' => ['string', 'max:10']
-            ],
-            (new ModelMetadata(ModelMetadataTestModelWithValidationRulesStub::class))
-                ->getValidationRules()
+        parent::setUp();
+        $app = new Application(
+            $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
         );
+        $app->withFacades();
+        Config::set('app.debug', true);
     }
 
-    public function testGetValidationRulesOfField()
+    public function dataProviderTestGetValidationRules(): array
     {
-        $this->assertEquals(
-            ['integer'],
-            (new ModelMetadata(ModelMetadataTestModelWithValidationRulesStub::class))
-                ->getValidationRules('id')
-        );
-        $this->assertEquals(
-            ['string', 'max:10'],
-            (new ModelMetadata(ModelMetadataTestModelWithValidationRulesStub::class))
-                ->getValidationRules('foo')
-        );
+        return [
+            [
+                fn() => (new ModelMetadata(ModelMetadataTestFirst::class))->getValidationRules(),
+                ['id' => ['integer'], 'foo' => ['string', 'max:10']],
+                null,
+            ],
+            [
+                fn() => (new ModelMetadata(ModelMetadataTestFirst::class))->getValidationRules('foo'),
+                ['string', 'max:10'],
+                null,
+            ],
+            [
+                fn() => (new ModelMetadata(ModelMetadataTestFirst::class))->getValidationRules('id'),
+                ['integer'],
+                null,
+            ],
+            [
+                fn() => new ModelMetadata(ModelMetadataTestSecond::class),
+                null,
+                IncorrectCaseOfPropertyVariableNameException::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderTestGetValidationRules
+     */
+    public function testGetValidationRules($getMetadataFunc, $expectResult, $expectException)
+    {
+        if ($expectException) {
+            $this->expectException($expectException);
+        }
+
+        $result = $getMetadataFunc();
+
+        if ($expectResult) {
+            $this->assertEquals($expectResult, $result);
+        }
     }
 
 }
@@ -41,7 +70,15 @@ class ModelMetadataTest extends TestCase
  * @property $id  {@property-type field} {@validation-rules integer}
  * @property $foo {@property-type field} {@validation-rules string|max:10}
  */
-class ModelMetadataTestModelWithValidationRulesStub extends Model
+class ModelMetadataTestFirst extends Model
+{
+
+}
+
+/**
+ * @property $fooBar  {@property-type field}
+ */
+class ModelMetadataTestSecond extends Model
 {
 
 }
