df78dcb547ef6d82ebd49862532af0c65f16c227
glebstorozhenko <ro.rollsover@gmail.com>
2021-10-07 19:33:09 +0700

    Fixup code style.

--- src/Centrifugo/config/broadcasting.php
+++ src/Centrifugo/config/broadcasting.php
@@ -5,29 +5,29 @@ declare(strict_types=1);
 return [
 
     /*
-        |--------------------------------------------------------------------------
-        | Default Broadcaster
-        |--------------------------------------------------------------------------
-        |
-        | This option controls the default broadcaster that will be used by the
-        | framework when an event needs to be broadcast. You may set this to
-        | any of the connections defined in the "connections" array below.
-        |
-        | Supported: "pusher", "redis", "log", "null"
-        |
+    |--------------------------------------------------------------------------
+    | Default Broadcaster
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default broadcaster that will be used by the
+    | framework when an event needs to be broadcast. You may set this to
+    | any of the connections defined in the "connections" array below.
+    |
+    | Supported: "pusher", "redis", "log", "null"
+    |
     */
 
     'default' => env('BROADCAST_DRIVER', 'centrifugo'),
 
     /*
-        |--------------------------------------------------------------------------
-        | Broadcast Connections
-        |--------------------------------------------------------------------------
-        |
-        | Here you may define all of the broadcast connections that will be used
-        | to broadcast events to other systems or over websockets. Samples of
-        | each available type of connection are provided inside this array.
-        |
+    |--------------------------------------------------------------------------
+    | Broadcast Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of the broadcast connections that will be used
+    | to broadcast events to other systems or over websockets. Samples of
+    | each available type of connection are provided inside this array.
+    |
     */
 
     'connections' => [
