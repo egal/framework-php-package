2fed0b7d506234ffeb311d138e9682e6a48cfde5
Аня <Sytuagina@sputnikfund.ru>
2021-10-15 16:12:13 +0700

    [exception-classifier] Fix phpcs

--- src/Core/Exceptions/NoInternalCodeSetException.php
+++ src/Core/Exceptions/NoInternalCodeSetException.php
@@ -1,12 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Egal\Core\Exceptions;
 
 use Exception;
 
 class NoInternalCodeSetException extends Exception
 {
+
     protected $message = 'No internal code set for InternalException!';
 
     protected $code = 500;
+
 }
