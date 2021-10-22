e19decdfe991972fb62273f1fbb98036362adf2e
Аня <Sytuagina@sputnikfund.ru>
2021-10-22 18:11:11 +0700

    [add_validating_event] add events validating/validated

--- src/Model/Traits/UsesValidator.php
+++ src/Model/Traits/UsesValidator.php
@@ -18,26 +18,38 @@ trait UsesValidator
     protected static function bootUsesValidator(): void
     {
         static::saving(static function (Model $entity): void {
-            // Получаем validation rules
-            // всех атрибутов если объект новый,
-            // только измененных атрибутов если объект обновляется.
-            //
-            // Получение validation rules только измененных атрибутов происходит
-            // путем получения пересечения всех validation rules и измененный атрибутов по ключам.
-            $validationRules = $entity->exists
-                ? array_intersect_key($entity->getValidationRules(), $entity->getDirty())
-                : $entity->getValidationRules();
-
-            // Применяем полученные validation rules на все атрибуты модели.
-            $validator = Validator::make($entity->getAttributes(), $validationRules);
-
-            if ($validator->fails()) {
-                $exception = new ValidateException();
-                $exception->setMessageBag($validator->errors());
-
-                throw $exception;
-            }
+            $entity->fireModelEvent('validating', true);
+            self::validate($entity);
+            $this->fireModelEvent('validated', true);
         });
+    }
+
+    /**
+     * @param Model $entity
+     * @throws ValidateException
+     * @throws \ReflectionException
+     */
+    protected static function validate(Model $entity): void
+    {
+        // Получаем validation rules
+        // всех атрибутов если объект новый,
+        // только измененных атрибутов если объект обновляется.
+        //
+        // Получение validation rules только измененных атрибутов происходит
+        // путем получения пересечения всех validation rules и измененный атрибутов по ключам.
+        $validationRules = $entity->exists
+            ? array_intersect_key($entity->getValidationRules(), $entity->getDirty())
+            : $entity->getValidationRules();
+
+        // Применяем полученные validation rules на все атрибуты модели.
+        $validator = Validator::make($entity->getAttributes(), $validationRules);
+
+        if ($validator->fails()) {
+            $exception = new ValidateException();
+            $exception->setMessageBag($validator->errors());
+
+            throw $exception;
+        }
     }
 
     /**
