d28701b7fa16cda3a8be55f55c0ee372fda6488b
annstgn <91238426+annstgn@users.noreply.github.com>
2021-11-08 17:56:22 +0700

    Fix: InternalException constructor. (#165)

--- src/Exception/InternalException.php
+++ src/Exception/InternalException.php
@@ -5,23 +5,11 @@ declare(strict_types=1);
 namespace Egal\Exception;
 
 use Exception;
-use Throwable;
 
 abstract class InternalException extends Exception implements HasInternalCode
 {
 
-    protected string $internalCode;
-
-    public function __construct($message = '', $code = 0, $internalCode = null, ?Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-
-        if (isset($internalCode)) {
-            $this->internalCode = $internalCode;
-        } elseif (!isset($this->internalCode)) {
-            throw new NoInternalCodeSetException();
-        }
-    }
+    protected string $internalCode = '';
 
     public function getInternalCode(): string
     {
