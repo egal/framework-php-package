78f62f39bc9ed2b1edd4e8bd7a0a8de9c1c0c46c
rollsover <51847757+rollsover@users.noreply.github.com>
2021-11-08 11:03:30 +0700

    Update InstanceForAction.php

--- src/Model/Traits/InstanceForAction.php
+++ src/Model/Traits/InstanceForAction.php
@@ -5,22 +5,13 @@ declare(strict_types=1);
 namespace Egal\Model\Traits;
 
 /**
- * Trait InstanceForAction
- *
- * @package Egal\Model\Traits
  * @mixin \Egal\Model\Model
  */
 trait InstanceForAction
 {
 
-    /**
-     * Mark of the current instance as being used in actions.
-     */
     protected bool $isInstanceForAction = false;
 
-    /**
-     * Mark the current instance as being used in actions.
-     */
     public function makeIsInstanceForAction(): self
     {
         $this->isInstanceForAction = true;
@@ -39,9 +30,6 @@ trait InstanceForAction
         return $instance;
     }
 
-    /**
-     * Make new instance of current class as being used in actions.
-     */
     protected static function newInstanceForAction(): self
     {
         $instance = new static();
