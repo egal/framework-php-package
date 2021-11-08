147543eb009a6e764933fda292a3f01f5bd41084
Аня <Sytuagina@sputnikfund.ru>
2021-11-08 18:10:07 +0700

    [test_filter_by_null] fix tests

--- tests/Model/ModelActionGetItemsFilterTest.php
+++ tests/Model/ModelActionGetItemsFilterTest.php
@@ -366,6 +366,7 @@ class ModelActionGetItemsFilterTest extends TestCase
  * @property int    $id                           {@property-type field}  {@prymary-key}
  * @property string $name       Название          {@property-type field}  {@validation-rules string}
  * @property string $count      Количество        {@property-type field}  {@validation-rules int}
+ * @property string $sale       Скидка          {@property-type field}  {@validation-rules int}
  * @property Carbon $created_at                   {@property-type field}  {@validation-rules date}
  * @property Carbon $updated_at                   {@property-type field}  {@validation-rules date}
  *
