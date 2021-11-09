ff6b2751305432e93f6c136e71eb1f284a94529a
rollsover <51847757+rollsover@users.noreply.github.com>
2021-11-09 11:59:42 +0700

    Update TokenExpiredException.php

--- src/Auth/Exceptions/TokenExpiredException.php
+++ src/Auth/Exceptions/TokenExpiredException.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Egal\Auth\Exceptions;
 
 use Egal\Exception\InternalException;
+use Illuminate\Support\Str;
 
 class TokenExpiredException extends InternalException
 {
 
     protected $code = 401;
 
-    protected string $internalCode = 'Token expired';
+    protected string $internalCode = 'token_expired';
 
     public static function make(string $tokenType): self
     {
         $result = new static();
-        $result->message = 'Token ' . $tokenType . ' expired!';
-        $result->internalCode = $tokenType . ' expired';
+        $result->message = 'Token ' . Str::upper($tokenType) . ' expired!';
+        $result->internalCode = $tokenType . '_expired';
 
         return $result;
     }
