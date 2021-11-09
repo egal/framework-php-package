40760028b87461ded3626da6f40a209d40a19e5d
Аня <Sytuagina@sputnikfund.ru>
2021-11-09 11:06:35 +0700

    [internal_code] add internal code to TokenExpiredException

--- src/Auth/Exceptions/TokenExpiredException.php
+++ src/Auth/Exceptions/TokenExpiredException.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace Egal\Auth\Exceptions;
 
-use Exception;
+use Egal\Exception\InternalException;
 
-class TokenExpiredException extends Exception
+class TokenExpiredException extends InternalException
 {
 
     protected $code = 401;
+    protected string $internalCode = 'Token expired';
 
     public static function make(string $tokenType): self
     {
         $result = new static();
         $result->message = 'Token ' . $tokenType . ' expired!';
+        $result->internalCode = $tokenType . ' expired';
 
         return $result;
     }
