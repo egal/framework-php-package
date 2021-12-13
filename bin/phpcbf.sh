b1d78b142d0055160cb9973e30299fe31275a7c6
Аня <Sytuagina@sputnikfund.ru>
2021-12-13 11:59:40 +0700

    ...

--- bin/phpcbf.sh
+++ bin/phpcbf.sh
@@ -50,7 +50,7 @@ if [ $DIFFS == TRUE ]; then
     COMMAND_ADDITIONAL_LINE="${COMMAND_ADDITIONAL_LINE} ${FILE}"
 fi
 
-docker run -it --rm \
+docker run --rm \
     --user "$(id -u):$(id -g)" \
     --workdir "${WORKDIR}" \
     --entrypoint "vendor/bin/phpcbf" \
