f39649bef2251d99ed6b4546d9a31b841427b854
Аня <Sytuagina@sputnikfund.ru>
2022-01-10 14:24:25 +0700

    Update: init EnumModel.

--- src/Model/EnumModel.php
+++ src/Model/EnumModel.php
@@ -2,15 +2,59 @@
 
 namespace Egal\Model;
 
-use ReflectionClass;
+use ReflectionException;
 
 abstract class EnumModel
 {
 
+    protected static array $cache = [];
+
+    public static function descriptions(): array
+    {
+        return [];
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public static function toArray()
+    {
+        $class = static::class;
+
+        if (!isset(static::$cache[$class])) {
+            $items = [];
+
+            $reflection = new \ReflectionClass($class);
+            $keyValuesArray = $reflection->getConstants();
+
+            foreach ($keyValuesArray as $key => $value) {
+                $item['key'] = $key;
+                $item['value'] = $value;
+                $item['description'] = static::descriptions()[$value];
+                $items[] = $item;
+            }
+
+            static::$cache[$class] = $items;
+        }
+
+        return static::$cache[$class];
+    }
+
     public static function actionGetItems(): array
     {
-        $reflectionClass = new ReflectionClass(get_called_class());
-        return $reflectionClass->getConstants();
+       return static::toArray();
+    }
+
+    public static function actionGetItem($keyValue): array
+    {
+        return array_filter(static::toArray(), function($value, $key) use ($keyValue) {
+            return $key == 'key' || $value == $keyValue;
+        }, ARRAY_FILTER_USE_BOTH);
+    }
+
+    public static function actionGetCount(): array
+    {
+        return ['count' => count(static::toArray())];
     }
 
 }
