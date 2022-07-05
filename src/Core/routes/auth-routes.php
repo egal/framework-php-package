bdb698869d13f481f393c992670a7fdbbd21bfa3
Аня <Sytuagina@sputnikfund.ru>
2022-07-05 15:15:41 +0700

    Update: No content responses.

--- src/Core/routes/auth-routes.php
+++ src/Core/routes/auth-routes.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [\Egal\Core\Auth\Controller::class, 'register']);
-Route::post('/login', [\Egal\Core\Auth\Controller::class, 'login']);
+Route::post('/login', [\Egal\Core\Auth\Controller::class, 'login'])->middleware(['session', 'api']);
