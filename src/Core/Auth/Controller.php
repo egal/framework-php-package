7b82bd435d34f6fc73dfca7acd78f9f933c4272b
Аня <Sytuagina@sputnikfund.ru>
2022-07-11 14:02:14 +0700

    Update: Add refreshToken api.

--- src/Core/Auth/Controller.php
+++ src/Core/Auth/Controller.php
@@ -95,25 +95,29 @@ class Controller extends BaseController
 
     public function refreshToken(Request $request)
     {
-        // TODO обновление токена
-        switch ($request->header('Authorization-Type')) {
-            case AuthorizationType::Cookie->value:
-                // TODO хранение сессии в redis
-                $user = RefreshToken::fromJWT($request->session()->get('refresh_token'))->getUser();
-                $accessToken = AccessToken::fromUser($user)->generateJWT();
-                $refreshToken = (new RefreshToken())->generateJWT();
-                $request->session()->put('access_token', $accessToken);
-                $request->session()->put('refresh_token', $refreshToken);
-                return response()->noContent()->setStatusCode(Response::HTTP_OK);
-            case AuthorizationType::Header->value:
-                $user = RefreshToken::fromJWT($request->header('Refresh-Token'))->getUser();
-                $accessToken = AccessToken::fromUser($user)->generateJWT();
-                $refreshToken = (new RefreshToken())->generateJWT();
-                $tokens = ['access_token' => $accessToken, 'refresh_token' => $refreshToken];
-                return response()->json($tokens)->setStatusCode(Response::HTTP_OK);
-            default:
-                // TODO отдельный exception
-                throw new Exception("Not specified correct 'Authorization-Type' header!", Response::HTTP_BAD_REQUEST);
+        try {
+            switch ($request->header('Authorization-Type')) {
+                case AuthorizationType::Cookie->value:
+                    // TODO хранение сессии в redis
+                    $user = RefreshToken::fromJWT($request->session()->get('refresh_token'))->getUser();
+                    $accessToken = AccessToken::fromUser($user)->generateJWT();
+                    $refreshToken = (new RefreshToken())->generateJWT();
+                    $request->session()->put('access_token', $accessToken);
+                    $request->session()->put('refresh_token', $refreshToken);
+                    return response()->noContent()->setStatusCode(Response::HTTP_OK);
+                case AuthorizationType::Header->value:
+                    $user = RefreshToken::fromJWT($request->header('Refresh-Token'))->getUser();
+                    $accessToken = AccessToken::fromUser($user)->generateJWT();
+                    $refreshToken = (new RefreshToken())->generateJWT();
+                    $tokens = ['access_token' => $accessToken, 'refresh_token' => $refreshToken];
+                    return response()->json($tokens)->setStatusCode(Response::HTTP_OK);
+                default:
+                    // TODO отдельный exception
+                    throw new Exception("Not specified correct 'Authorization-Type' header!", Response::HTTP_BAD_REQUEST);
+            }
+
+        } catch (Exception $exception) {
+            return response()->json($this->getExceptionResponseData($exception))->setStatusCode($exception->getCode());
         }
     }
 
@@ -121,7 +125,6 @@ class Controller extends BaseController
     {
         return [
             'message' => $exception->getMessage(),
-            'code' => $exception->getCode(),
             'internal_code' => $exception instanceof HasInternalCode ? $exception->getInternalCode() : null,
             'data' => $exception instanceof HasData ? $exception->getData() : null
         ];
